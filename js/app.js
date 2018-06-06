c6e6f441ce69cebeba573278336bc5e97f92a568
saturn123 <satyanbasnet@gmail.com>
2018-06-05 18:37:01 -0700

    Display modal with "You won" message, resets the game

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ const cards = ["fa fa-diamond", "fa fa-diamond",
 const counter = document.querySelector(".moves");
 const deck = document.querySelector(".deck");
 const stars = document.querySelectorAll(".fa-star");
+const modal = document.querySelector(".modal");
+const restart = document.querySelector(".restart");
 let openCards = [];
 let matchedCards = [];
 let moves = 0;
@@ -42,19 +44,6 @@ function initGame() {
   });
   deck.innerHTML = generateCardHTML.join("");
 
-  //Resets the game once all the cards are matched and the gave is over
-  const restart = document.querySelector(".restart");
-  restart.addEventListener("click", function() {
-    //Clears deck
-    deck.innerHTML = "";
-
-    //Start new Game
-    initGame();
-
-    //Reset matched card array
-    matchedCards = [];
-  });
-
   //Resets moves
   moves = 0;
   counter.innerHTML = moves;
@@ -65,8 +54,15 @@ function initGame() {
     stars[i].style.visibility = 'visible';
   }
 
+  //hide Modal
+  modal.classList.add("hide");
+
+  //
+  seconds = 0;
+  minutes = 0;
+
   //Play game
-    playGame();
+  playGame();
 }
 
 //set up the event listener for a card. If a card is clicked: display the card's symbol and push the card in openCards array
@@ -88,8 +84,8 @@ function playGame() {
 
         //compare 2 opened cards
         compareCards(currentCard, previousCard);
-
-      } else if (openCards < 2) {
+        //Players restricted to click only 2 cards at a time
+      } else if (openCards < 2)  {
           card.classList.add("open", "show", "disable");
           openCards.push(this);
         }
@@ -97,16 +93,18 @@ function playGame() {
   });
 }
 
-
 function compareCards(currentCard, previousCard) {
   if (currentCard.innerHTML === previousCard.innerHTML) {
 
     // if the two cards match
     currentCard.classList.add("match");
     previousCard.classList.add("match");
-    matchedCards.push(currentCard, previousCard);
 
+    matchedCards.push(currentCard, previousCard);
     openCards = [];
+
+    //Check if all cards have been matched
+    gameOver();
 
     //if the two cards do not match, hide cards
   } else {
@@ -114,7 +112,7 @@ function compareCards(currentCard, previousCard) {
       currentCard.classList.remove("open", "show", "disable");
       previousCard.classList.remove("open", "show", "disable");
       openCards = [];
-    }, 500);
+    }, 750);
   }
   moveCounter();
 }
@@ -161,20 +159,19 @@ function startTimer() {
   }, 750);
 }
 
+function gameOver() {
+  if(matchedCards.length === cards.length) {
+    clearInterval(interval);
+    setTimeout(function(){
+      modal.classList.remove("hide");
+    }, 500);
+  }
+}
+
+//Resets the game once all the cards are matched and the game is over
+restart.addEventListener("click", function() {
+  window.location.reload();
+});
 
 
 initGame();
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
